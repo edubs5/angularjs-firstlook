11d7ef71171b9f26ac8083ae3b4ad314cc96adc8
Elmer Wu <elmerwu@gmail.com>
2014-08-07 16:46:25 -0700

    set default selection to rank

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,6 @@ var myApp = angular.module('myApp', []);
 myApp.controller('myController', ['$scope', '$http', function($scope, $http) {
   $http.get('js/data.json').success(function(data) {
     $scope.players = data;
+    $scope.playerOrder = "rank";
   });
 }]);
