9fbd9f3f3db241fcb08636ce2cbe033c4b667d6a
Elmer Wu <elmerwu@gmail.com>
2014-08-07 17:50:47 -0700

    add details partial

--- js/controllers.js
+++ js/controllers.js
@@ -6,3 +6,10 @@ playerControllers.controller('ListController', ['$scope', '$http', function($sco
     $scope.playerOrder = "rank";
   });
 }]);
+
+playerControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+  $http.get('js/data.json').success(function(data) {
+    $scope.players = data;
+    $scope.whichItem = $routeParams.itemId;
+  });
+}]);
